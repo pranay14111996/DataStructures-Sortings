ea956069740d0ddce7f40ce707f533926da286df
pranay14111996 <chinta.pranay1411@gmail.com>
2019-08-12 23:47:49 +0530

    initial commit of bubble sort

---
+++ src/main/java/com/mypractice/sorting/BubbleSort.java
@@ -0,0 +1,48 @@
+package com.mypractice.sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		/*Scanner s = new Scanner(System.in);
+		System.out.print("Enter array length :: ");
+		int l = s.nextInt();
+		int[] arr = new int[l];
+		System.out.print("\n Enter array elements :: ");
+		for (int i = 0; i < l; i++) {
+			arr[i] = s.nextInt();
+		}*/
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; 
+		doBubbleSort(arr,arr.length);
+		
+	}
+
+	private static void doBubbleSort(int[] arr, int l) {
+		boolean swapped = false;
+		
+		for(int i = 0 ; i < l-1;i++){
+			for( int j = 0;j<l-i-1;j++ ){
+				if(arr[j] > arr[j+1] ){
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+					swapped = true;
+				}
+			}
+			if(!swapped){
+				break;
+			}
+		}
+		System.out.print("\n After Sorting elements are :: ");
+		printArray(arr,l);
+		
+	}
+	private static void printArray(int arr[], int size) 
+    { 
+        int i; 
+        for (i = 0; i < size; i++) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+}
