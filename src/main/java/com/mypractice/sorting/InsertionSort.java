b4fb9a82afe975f8745dd198da68ecd70b11b16f
pranay1411 <pranaytheglazingfire@gmail.com>
2019-08-14 23:36:00 +0530

    Insertion Sort initial commit

---
+++ src/main/java/com/mypractice/sorting/InsertionSort.java
@@ -0,0 +1,37 @@
+package com.mypractice.sorting;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		/*Scanner s = new Scanner(System.in);
+		System.out.print("Enter array length :: ");
+		int l = s.nextInt();
+		int[] arr = new int[l];
+		System.out.print("\n Enter array elements :: ");
+		for (int i = 0; i < l; i++) {
+			arr[i] = s.nextInt();
+		}*/
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; 
+		doInsertionSort(arr,arr.length);
+		
+	}
+	private static void doInsertionSort(int[] arr, int length) {
+		for( int i = 1 ; i < length ;i++){
+			int key = arr[i];
+			int j = i -1;
+			while( j>=0 && arr[j] > key ){
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = key;
+		}
+		printArray(arr,length);
+		
+	}
+	private static void printArray(int arr[], int size) 
+    { 
+        int i; 
+        for (i = 0; i < size; i++) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+}
