e1c2dca5cb7bef8eb4ededda0a2a8d2f60322137
Chinta Pranay <chinta.pranay@intellectdesign.com>
2019-08-30 15:50:26 +0530

    TRINODE commit

--- src/main/java/com/mypractice/tries/TrieNode.java
+++ src/main/java/com/mypractice/tries/TrieNode.java
@@ -1,8 +1,15 @@
 package com.mypractice.tries;
 
+import java.util.Arrays;
+
 public class TrieNode {
 
 	boolean endOfWord = false;
 	TrieNode[] nodes = new TrieNode[26];
+	/*
+	 * @Override public String toString() { return "TrieNode [endOfWord=" +
+	 * endOfWord + ", nodes=" + Arrays.toString(nodes) + "]"; }
+	 */
+	
 	
 }
